23571ee577e6821d8cdf28286940147b57296594
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-19 10:33:01 -0300

    feat: add total tickets open and solved card

--- app/(dashboard)/painel/page.tsx
+++ app/(dashboard)/painel/page.tsx
@@ -7,6 +7,8 @@ import {
   HeaderTitle,
 } from "@/components/header";
 import TotalTickets from "./_components/total-tickets";
+import TotalTicketsOpen from "./_components/total-tickets-open";
+import TotalTicketsSolved from "./_components/total-tickets-solved";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -17,7 +19,7 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="m-8 flex w-full flex-col space-y-8 rounded-lg">
+    <div className="m-8 flex flex-col space-y-8 rounded-lg">
       <Header>
         <HeaderLeft>
           <HeaderTitle>Painel</HeaderTitle>
@@ -25,8 +27,10 @@ export default async function ProtectedPage() {
         </HeaderLeft>
       </Header>
 
-      <div className=" grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-3 gap-6">
         <TotalTickets />
+        <TotalTicketsOpen />
+        <TotalTicketsSolved />
       </div>
     </div>
   );
