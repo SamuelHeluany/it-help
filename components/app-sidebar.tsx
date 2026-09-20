caaf7e723aa3c9c9b3f9e2759cedeb45dffb9e03
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-20 11:00:00 -0300

    feat: add selected sidebar button page

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,34 +1,32 @@
-import { SidebarMenuButton } from "@/components/ui/sidebar";
+"use client";
 import { LayoutGridIcon, Ticket } from "lucide-react";
+import SidebarButton from "./sidebar-buttons";
 
 export function AppSidebar() {
   return (
     <div className="p w-64 bg-[#5C64E1]">
       <div className="px-8 py-6">
         <a href="/painel">
-          <span className="text-4xl font-bold flex">
+          <span className="text-2xl font-bold flex justify-center">
             <p className="text-[#7CDBFD]">IT</p>
             <p className="text-white">-HELP</p>
           </span>
         </a>
       </div>
       <div className="flex flex-col gap-2 p-2 ">
-        <SidebarMenuButton className="pb-3 cursor-pointer">
-          <a href="/painel" className="flex w-full items-center text-xl gap-2">
+        <SidebarButton href="/painel">
+          <div className="flex gap-2 items-center">
             <LayoutGridIcon color="white" />
-            <p className="text-white">Painel</p>
-          </a>
-        </SidebarMenuButton>
+            <p className="text-xl text-[#ffffff]">Painel</p>
+          </div>
+        </SidebarButton>
 
-        <SidebarMenuButton className="pb-3 cursor-pointer">
-          <a
-            href="/tickets"
-            className="flex w-full items-center text-[20px] gap-2"
-          >
+        <SidebarButton href="/tickets">
+          <div className="flex gap-2 items-center">
             <Ticket color="white" />
-            <p className="text-white">Tickets</p>
-          </a>
-        </SidebarMenuButton>
+            <p className="text-xl text-[#ffffff]">Tickets</p>
+          </div>
+        </SidebarButton>
       </div>
     </div>
   );
---
+++ components/sidebar-buttons.tsx
@@ -0,0 +1,22 @@
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+interface SidebarButtonProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+const SidebarButton = ({ children, href }: SidebarButtonProps) => {
+  const pathName = usePathname();
+  return (
+    <Button
+      variant={pathName === `${href}` ? "siderbarHover" : "ghost"}
+      className="justify-start gap-2"
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
+
+export default SidebarButton;
