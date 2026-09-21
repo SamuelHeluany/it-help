4f8b041d977a9cef4ba382c9580b014d0441d41e
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-21 13:55:36 -0300

    feat: add responsivity

--- app/(dashboard)/painel/page.tsx
+++ app/(dashboard)/painel/page.tsx
@@ -27,7 +27,7 @@ export default async function ProtectedPage() {
         </HeaderLeft>
       </Header>
 
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         <TotalTickets />
         <TotalTicketsOpen />
         <TotalTicketsSolved />
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,27 +4,27 @@ import SidebarButton from "./sidebar-buttons";
 
 export function AppSidebar() {
   return (
-    <div className="p w-64 bg-[#5C64E1]">
-      <div className="px-8 py-6">
-        <a href="/painel">
-          <span className="text-2xl font-bold flex justify-center">
+    <div className="w-16 md:w-64 min-h-screen transition-all duration-300 bg-[#5C64E1]">
+      <div className="px-2 md:px-8 py-6">
+        <a href="/painel" className="grid justify-center items-center">
+          <span className="text-sm grid md:text-2xl font-bold md:flex justify-center">
             <p className="text-[#7CDBFD]">IT</p>
-            <p className="text-white">-HELP</p>
+            <p className="text-white">HELP</p>
           </span>
         </a>
       </div>
       <div className="flex flex-col gap-2 p-2">
         <SidebarButton href="/painel">
-          <div className="flex gap-2 items-center">
-            <LayoutGridIcon color="#1D161E" />
-            <p className="text-xl text-mauve-900">Painel</p>
+          <div className="flex gap-2 items-center justify-center md:justify-start">
+            <LayoutGridIcon color="#1D161E" className="shrink-0" />
+            <p className="text-xl text-mauve-900 hidden md:block">Painel</p>
           </div>
         </SidebarButton>
 
         <SidebarButton href="/tickets">
-          <div className="flex gap-2 items-center">
-            <Ticket color="#1D161E" />
-            <p className="text-xl text-mauve-900">Tickets</p>
+          <div className="flex gap-2 items-center justify-center md:justify-start">
+            <Ticket color="#1D161E" className="shrink-0" />
+            <p className="text-xl text-mauve-900 hidden md:block">Tickets</p>
           </div>
         </SidebarButton>
       </div>
