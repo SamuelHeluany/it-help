9a7807a6a2225d71ea11a139f3a1e2917c3ac16f
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-23 09:27:22 -0300

    feat: add fast actions to painel page

--- app/(dashboard)/painel/page.tsx
+++ app/(dashboard)/painel/page.tsx
@@ -9,6 +9,9 @@ import {
 import TotalTickets from "./_components/total-tickets";
 import TotalTicketsOpen from "./_components/total-tickets-open";
 import TotalTicketsSolved from "./_components/total-tickets-solved";
+import CreateTicket from "../tickets/_components/create-ticket";
+import { Lock, Plus } from "lucide-react";
+import Link from "next/link";
 
 export const metadata = {
   title: "Painel",
@@ -21,6 +24,10 @@ export default async function ProtectedPage() {
   if (error || !data?.claims) {
     redirect("/login");
   }
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   return (
     <div className="mt-20 mr-3 ml-3 mb-3flex flex-col space-y-8 rounded-lg">
@@ -35,6 +42,27 @@ export default async function ProtectedPage() {
         <TotalTickets />
         <TotalTicketsOpen />
         <TotalTicketsSolved />
+      </div>
+
+      <Header>
+        <HeaderLeft>
+          <HeaderTitle>Ações rápidas</HeaderTitle>
+        </HeaderLeft>
+      </Header>
+
+      <div className="gap-2 flex">
+        <CreateTicket userId={user?.id}>
+          <span className="bg-blue-950 flex text-white md:w-30 md:h-10 items-center cursor-pointer rounded-sm justify-center hover:bg-blue-800 w-25 h-8 text-sm gap-0.5">
+            <Plus className="w-4 h-4 md:h-4.5 md:w-4.5" />
+            Criar ticket
+          </span>
+        </CreateTicket>
+        <Link href="/update-password" className="flex gap-1">
+          <button className="bg-blue-950 flex text-white md:w-30 md:h-10 items-center cursor-pointer rounded-sm justify-center hover:bg-blue-800 w-25 h-8 text-sm gap-0.5">
+            <Lock className="w-4 h-4 md:h-4.5 md:w-4.5" />
+            <p className="text-[12px] sm:text-sm">Alterar senha</p>
+          </button>
+        </Link>
       </div>
     </div>
   );
